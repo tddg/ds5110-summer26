7b463e4fbef500576372d4520fbd34856f3d1f85
Yue Cheng <kevinchan4007@gmail.com>
2026-06-07 23:05:59 -0400

    added lec11's link from Ed

--- app.js
+++ app.js
@@ -303,7 +303,7 @@ presentation.</p>
       { week: 3, date: "Fri Jun 5", topic: "Studio / Work Session", materials: [], hideMaterials:true, notes: "Assignment 2 due" },
       { week: 4, date: "Mon Jun 8", topic: "Model hub compression", 
 		materials: [
-			{ text: "Lec11: AI model hub compression", href: "assets/docs/lec11-model-hub-compression.pdf" },
+			{ text: "Lec11: AI model hub compression", href: "https://edstem.org/us/courses/98636/discussion/8119360" },
 		], notes: "" },
       { week: 4, date: "Tue Jun 9", topic: "GPU-efficient computational notebooks", materials: [], notes: "" },
       { week: 4, date: "Wed Jun 10", topic: "Working day", materials: [], hideMaterials:true, notes: "Presentation slides due at 5 PM" },
